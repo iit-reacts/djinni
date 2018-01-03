20093f748334f391c5998aef7be8305df91e1322
Leandro Freitas <lfreitas@reacts.com>
2018-01-03 12:56:54 -0500

    Add WcharTest

--- test-suite/run_djinni.ps1
+++ test-suite/run_djinni.ps1
@@ -6,12 +6,12 @@ $base_dir = $PSScriptRoot
 $djinni_dir = "$(Split-Path -Parent $base_dir)\src"
 
 $temp_out = "$base_dir\djinni-output-temp"
-$in = "$base_dir\djinni\py_all.djinni"
-#$wchar_in = "$base_dir\djinni\wchar_test.djinni"
+$in = "$base_dir\djinni\all.djinni"
+$wchar_in = "$base_dir\djinni\wchar_test.djinni"
 
 $temp_out_relative = "djinni-output-temp"
-$in_relative = "djinni\py_all.djinni"
-#$wchar_in_relative = "djinni\wchar_test.djinni"
+$in_relative = "djinni\all.djinni"
+$wchar_in_relative = "djinni\wchar_test.djinni"
 
 $cpp_out = "$base_dir\generated-src\cpp"
 $cs_out = "$base_dir\generated-src\cs"
@@ -26,11 +26,11 @@ echo "djinni_dir = $djinni_dir"
 
 echo "temp_out = $temp_out"
 echo "in = $in"
-#echo "wchar_in = $wchar_in"
+echo "wchar_in = $wchar_in"
 
 echo "temp_out_relative = $temp_out_relative"
 echo "in_relative = $in_relative"
-#echo "wchar_in_relative = $wchar_in_relative"
+echo "wchar_in_relative = $wchar_in_relative"
 
 echo "cpp_out = $cpp_out"
 echo "cs_out = $cs_out"
@@ -52,40 +52,40 @@ bash.exe $djinni_dir\build
 
 # TODO check if it was already generated...
 
-#if (Test-Path $temp_out) {
-#    # Directory exists.
-#    Remove-Item -Recurse -Force $temp_out
-#}
+if (Test-Path $temp_out) {
+    # Directory exists.
+    Remove-Item -Recurse -Force $temp_out
+}
 
 cd $base_dir
 
 # Run Djinni generation
-#& "$base_dir\..\src\target\start.bat" `
-#    --cpp-out "$temp_out_relative/cpp" `
-#    --cpp-namespace testsuite `
-#    --ident-cpp-enum-type foo_bar `
-#    --cpp-optional-template "std::experimental::optional" `
-#    --cpp-optional-header "`"../../handwritten-src/cpp/optional.hpp`"" `
-#    --cpp-extended-record-include-prefix "../../handwritten-src/cpp/" `
-#    --cpp-use-wide-strings true `
-#    `
-#    --cs-out "$temp_out/cs" `
-#    --cs-namespace Djinni.TextSort `
-#    --cppcli-out "$temp_out/cppcli" `
-#    `
-#    --idl "$wchar_in_relative"
+& "$base_dir\..\src\target\start.bat" `
+    --cpp-out "$temp_out_relative/cpp" `
+    --cpp-namespace testsuite `
+    --ident-cpp-enum-type foo_bar `
+    --cpp-optional-template "std::experimental::optional" `
+    --cpp-optional-header "\`"../../handwritten-src/cpp/optional.hpp\`"" `
+    --cpp-extended-record-include-prefix "../../handwritten-src/cpp/" `
+    --cpp-use-wide-strings true `
+    `
+    --cs-out "$temp_out/cs" `
+    --cs-namespace Djinni.TestSuite `
+    --cppcli-out "$temp_out/cppcli" `
+    `
+    --idl "$wchar_in_relative"
 
-#& "$base_dir\..\src\target\start.bat" `
-#    --cpp-out "$temp_out_relative/cpp" `
-#    --cpp-namespace testsuite `
-#    --ident-cpp-enum-type foo_bar `
-#    --cpp-optional-template "std::experimental::optional" `
-#    --cpp-optional-header "`"../../handwritten-src/cpp/optional.hpp`"" `
-#    --cpp-extended-record-include-prefix "../../handwritten-src/cpp/" `
-#    `
-#    --cs-out "$temp_out/cs" `
-#    --cs-namespace Djinni.TestSuite `
-#    --cppcli-out "$temp_out/cppcli" `
-#    `
-#    --idl "$in_relative" `
-#    --idl-include-path "djinni/vendor"
+& "$base_dir\..\src\target\start.bat" `
+    --cpp-out "$temp_out_relative/cpp" `
+    --cpp-namespace testsuite `
+    --ident-cpp-enum-type foo_bar `
+    --cpp-optional-template "std::experimental::optional" `
+    --cpp-optional-header "\`"../../handwritten-src/cpp/optional.hpp\`"" `
+    --cpp-extended-record-include-prefix "../../handwritten-src/cpp/" `
+    `
+    --cs-out "$temp_out/cs" `
+    --cs-namespace Djinni.TestSuite `
+    --cppcli-out "$temp_out/cppcli" `
+    `
+    --idl "$in_relative" `
+    --idl-include-path "djinni/vendor"
