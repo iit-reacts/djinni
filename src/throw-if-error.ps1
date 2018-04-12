ca11ede21210e800234a375210284be1b0de484a
Leandro Freitas <lfreitas@reacts.com>
2018-04-12 11:41:04 -0400

    Abort Powershell script if Djinni fails

---
+++ src/throw-if-error.ps1
@@ -0,0 +1,15 @@
+﻿#---------------------------------------------------------------------------------------------------
+# Function definition
+#---------------------------------------------------------------------------------------------------
+
+function ThrowIfError {
+    Param(
+        [Parameter(Mandatory=$True)]
+        [string]$message
+    )
+
+    if($LastExitCode -ne 0)
+    {
+        throw [System.Exception] "$($MyInvocation.ScriptName):$($MyInvocation.ScriptLineNumber): ERROR: $message"
+    }
+}
