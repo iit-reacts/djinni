c542dd6683fff019045f63110a03edd22e834e88
Leandro Freitas <lfreitas@reacts.com>
2019-05-03 09:33:56 -0400

    Rebuild Djinni dependents when any dependency is changed

--- src/build.ps1
+++ src/build.ps1
@@ -2,21 +2,18 @@ $base_dir = $PSScriptRoot
 $loc = "$base_dir/$($MyInvocation.MyCommand.Name)"
 
 $need_to_build = $false
-$dependencies = @("build.sbt", "project/plugins.sbt", "source")
-$launcher = "target/start.bat"
+$dependencies = @("build.ps1", "build.sbt", "project/plugins.sbt") | ForEach-Object { Join-Path -Path $base_dir -ChildPath $_ } | Get-Item
+$dependencies += (Get-ChildItem "$base_dir/source")
 
+$launcher = "target/start.bat"
 # Run the build if necessary.
 if (-Not (Test-Path "$base_dir/$launcher")) {
     # If the launcher file doesn't exist, the build was never even run.
     $need_to_build = $true
 } else {
     # If the launcher file isn't newer than all the build dependencies, the build needs to be rerun.
-    $dependencies_full_path = @("$loc")
     foreach ($dependency in $dependencies) {
-        $dependencies_full_path += @("$base_dir/$dependency")
-    }
-    foreach ($dependency in $dependencies_full_path) {
-        if ((ls $dependency).LastWriteTime -ge (ls "$base_dir/$launcher").LastWriteTime) {
+        if ($dependency.LastWriteTime -ge (ls "$base_dir/$launcher").LastWriteTime) {
             $need_to_build = $true
             break
         }
